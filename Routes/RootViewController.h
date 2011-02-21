b65d399620b04e43c549a9e9eec516f091678d10
Stefan Arentz <stefan@arentz.ca>
2011-02-21 17:44:41 -0500

    Initial Commit

---
+++ Routes/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Routes
+//
+//  Created by Stefan Arentz on 11-02-21.
+//  Copyright 2011 Arentz Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ Routes/RoutesAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  RoutesAppDelegate.h
+//  Routes
+//
+//  Created by Stefan Arentz on 11-02-21.
+//  Copyright 2011 Arentz Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoutesAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
